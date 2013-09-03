aea5b3fe6f76435e9d739d0e56dbfec9c3f16330
achawla2012 <chawl007@umn.edu>
2013-09-02 20:12:09 -0500

    Update ServerUDPListen.java

--- src/ServerUDPListen.java
+++ src/ServerUDPListen.java
@@ -1,24 +1,13 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 import java.lang.*;
 import java.net.*;
-
 public class ServerUDPListen implements Runnable{
-    
     Thread t;
     ServerUDPListen() throws SocketException{
       t = new Thread(this, "Server Ping Listening Thread");
       t.start(); // Start the thread
-      
    }
-    
     public void run() {
       try {  
-          
-          
-          
           DatagramSocket UDPListenSocket = new DatagramSocket(9800);
           byte[] buffer = new byte[1024];
           while(true) {
@@ -26,9 +15,7 @@ public class ServerUDPListen implements Runnable{
               DatagramPacket Packet = new DatagramPacket(buffer, buffer.length);
               //System.out.println ("Waiting for Ping from Registry Server at 9800..");
               UDPListenSocket.receive(Packet);
-              
               String ReceivedString = new String(Packet.getData());
-              
               InetAddress IPAddress = Packet.getAddress();
               int port = Packet.getPort(); 
               buffer = ReceivedString.getBytes();
@@ -37,10 +24,8 @@ public class ServerUDPListen implements Runnable{
               UDPListenSocket.send(Packet);
                // System.out.println("Pinged Back");
           } 
-         
      } catch (Exception e) {
          System.out.println("Server Listening Socket Exception");
      }
-     
    }
 }
