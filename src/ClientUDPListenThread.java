891b7f1c8ae372be40e0478513d9a6937c42e84e
achawla2012 <chawl007@umn.edu>
2013-09-02 20:28:16 -0500

    Update ClientUDPListenThread.java

--- src/ClientUDPListenThread.java
+++ src/ClientUDPListenThread.java
@@ -1,27 +1,21 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 import java.lang.*;
 import java.net.*;
 import java.util.*;
-
 public class ClientUDPListenThread implements Runnable{
     Thread t;
     LinkedList<String> tt ;
+
     ClientUDPListenThread(LinkedList<String> a) {
         tt = a;
     }
-    
+
     ClientUDPListenThread() {
       t = new Thread(this, "Client Listening Thread");
       t.start(); // Start the thread
-   }
-    
+    }
+
     public void run() {
       try {
-          //Thread.currentThread().sleep(1000);
-          //System.out.println(tt.size());
           DatagramSocket UDPListenSocket = new DatagramSocket(Utility.client_listen_port);
           byte[] buffer = new byte[1024];
           while(true) {
@@ -33,17 +27,10 @@ public class ClientUDPListenThread implements Runnable{
               InetAddress IPAddress = Packet.getAddress();
               int port = Packet.getPort(); 
               System.out.println("Received Article from my group server : " + ReceivedString );
+              System.out.println("Exiting child thread.");
           }
-         /*for(int i = 5; i > 0; i--) {
-            System.out.println("Child Thread: " + i);
-            // Let the thread sleep for a while.
-            Thread.sleep(500);
-         }*/
-     } catch (Exception e) {
-         //System.err.println("Client exception: " + e.toString());  
-         //e.printStackTrace(); 
+       } catch (Exception e) {
          System.out.println("UDP Listening Socket Exception");
      }
-     System.out.println("Exiting child thread.");
    }
 }
