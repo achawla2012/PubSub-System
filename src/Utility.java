2a4ec8e52af3f851989abd86e4bc2c09493b53fb
achawla2012 <chawl007@umn.edu>
2013-09-02 20:11:21 -0500

    Update Utility.java

--- src/Utility.java
+++ src/Utility.java
@@ -1,34 +1,21 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
 import java.net.InetAddress;
 import java.net.NetworkInterface;
 import java.util.Enumeration;
 import java.net.*;
-
 public class Utility {
-    
-    
     public static int client_listen_port = 9878; 
     public static int max_clients = 10;
     public static int RMIRegistryPort = 5555;
-    
     public static String getIP() throws SocketException{
-        
         try
 		{
 			for (Enumeration<NetworkInterface> en = NetworkInterface.getNetworkInterfaces(); en.hasMoreElements();)
 			{
 				NetworkInterface intf = en.nextElement();
-			
 				for (Enumeration<InetAddress> enumIpAddr = intf.getInetAddresses(); enumIpAddr.hasMoreElements();)
 				{
 					InetAddress inetAddress = enumIpAddr.nextElement();
-
 					//System.out.println(inetAddress.getHostAddress().toString());
-									
 					if (!inetAddress.isLoopbackAddress() && !inetAddress.isLinkLocalAddress())// && inetAddress.isSiteLocalAddress())
 					{
 						//System.out.println("return");
@@ -42,7 +29,6 @@ public class Utility {
 			System.out.println(ex.getMessage());
 			return null;
 		}
-
 		return null;
     }
 }
